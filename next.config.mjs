9bd282107261a939fe689765e4a2d9d20e1e35de
dandelion-huang <dandelion820308@gmail.com>
2024-06-28 18:04:32 +0800

    feat(next.config.mjs): update configs for anchor-pki auto certification

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+import autoCert from 'anchor-pki/auto-cert/integrations/next';
+
+const withAutoCert = autoCert({ enabledEnv: 'development' });
 const nextConfig = {};
 
-export default nextConfig;
+export default withAutoCert(nextConfig);
