3ac990aab28b428ccf45ba9ee8543493a03f3568
dandelion-huang <dandelion820308@gmail.com>
2024-07-14 13:45:30 +0800

    feat(next.config.mjs): setup bundle-analyzer and next-compose-plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
+import bundleAnalyzer from '@next/bundle-analyzer';
 import autoCert from 'anchor-pki/auto-cert/integrations/next';
+import withPlugins from 'next-compose-plugins';
 
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: process.env.ANALYZE === 'true',
+});
 const withAutoCert = autoCert({ enabledEnv: 'development' });
-const nextConfig = {};
 
-export default withAutoCert(nextConfig);
+export default withPlugins([withBundleAnalyzer, withAutoCert]);
