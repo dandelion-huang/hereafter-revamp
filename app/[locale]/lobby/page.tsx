4b7f1c5d24e2de581af338b4675cef486b5d6534
dandelion-huang <dandelion820308@gmail.com>
2024-07-14 14:59:00 +0800

    fix(app/[locale]/lobby): add a simple lobby page
    Fix the server responded with a status of 404 (Not Found) for prefetched lobby page.

---
+++ app/[locale]/lobby/page.tsx
@@ -0,0 +1,7 @@
+export default function Lobby() {
+  return (
+    <div className="flex h-dvh min-h-svh w-full items-center justify-center text-center font-m-plus text-8xl text-white">
+      Lobby
+    </div>
+  );
+}
