eddea77f39a6d9ed30318a9925659c0789475264
dandelion-huang <dandelion820308@gmail.com>
2024-07-19 17:49:12 +0800

    feat(next.config.mjs): setup @react-aria/optimize-locales-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
+import optimizeLocales from '@react-aria/optimize-locales-plugin';
 import autoCert from 'anchor-pki/auto-cert/integrations/next';
 import withPlugins from 'next-compose-plugins';
 
+const withAutoCert = autoCert({ enabledEnv: 'development' });
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
   openAnalyzer: process.env.ANALYZE === 'true',
 });
-const withAutoCert = autoCert({ enabledEnv: 'development' });
 
-export default withPlugins([withBundleAnalyzer, withAutoCert]);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.plugins.push(
+      optimizeLocales.webpack({
+        locales: ['en-US', 'zh-TW'],
+      })
+    );
+
+    return config;
+  },
+};
+
+export default withPlugins([withBundleAnalyzer, withAutoCert], nextConfig);
