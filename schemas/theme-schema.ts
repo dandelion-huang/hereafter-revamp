41b900b370650d3380f53f2d5b88b5d53506f889
dandelion-huang <dandelion820308@gmail.com>
2024-07-19 13:44:28 +0800

    feat(schemas): function parseTheme

--- schemas/theme-schema.ts
+++ schemas/theme-schema.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 
-import { THEMES as themes } from '@/constants/theme';
+import {
+  DEFAULT_THEME as defaultTheme,
+  THEMES as themes,
+} from '@/constants/theme';
 
 import { type Theme } from '@/types/theme';
 
@@ -8,4 +11,14 @@ export const themeSchema = z.enum(themes);
 
 export function isValidTheme(theme: string): theme is Theme {
   return themeSchema.safeParse(theme).success;
+}
+
+export function parseTheme(theme: string) {
+  const result = themeSchema.safeParse(theme);
+
+  if (result.success) {
+    return result.data;
+  }
+
+  return defaultTheme;
 }
