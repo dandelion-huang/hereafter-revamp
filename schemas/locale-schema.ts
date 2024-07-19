360f12f4863e0939bf81e1fd6d559d9465711304
dandelion-huang <dandelion820308@gmail.com>
2024-07-19 13:45:19 +0800

    feat(schemas): function isTwLocale

--- schemas/locale-schema.ts
+++ schemas/locale-schema.ts
@@ -9,6 +9,10 @@ export const firstSegmentWithSlashRegex = /^\/([^/]+)/;
 export const validLocaleRegex = /^[a-z]{2}(-[A-Z]{2})?$/; // ISO 3166
 export const localeSchema = z.enum(locales);
 
+export function isTwLocale(locale: Locale) {
+  return locale === 'zh-TW';
+}
+
 export function isValidLocale(locale: string): locale is Locale {
   return localeSchema.safeParse(locale).success;
 }
