17381524943f708a7eb84758b7e36226e3da5ee3
dandelion-huang <dandelion820308@gmail.com>
2024-07-24 13:48:04 +0800

    build(app/fonts): minimize necessary font-weight
    Just want to optimize the performance of the website.

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,7 +3,7 @@ import { M_PLUS_Rounded_1c, Noto_Sans, Noto_Sans_TC } from 'next/font/google';
 export const notoSans = Noto_Sans({
   display: 'swap',
   preload: true,
-  weight: ['100', '200', '400', '500', '700', '900'],
+  weight: ['300', '400', '500'],
   subsets: ['latin'],
   variable: '--font-noto-sans',
 });
@@ -11,7 +11,7 @@ export const notoSans = Noto_Sans({
 export const notoSansTC = Noto_Sans_TC({
   display: 'swap',
   preload: true,
-  weight: ['100', '200', '400', '500', '700', '900'],
+  weight: ['300', '400', '500'],
   subsets: ['latin'],
   variable: '--font-noto-sans-tc',
 });
@@ -19,7 +19,7 @@ export const notoSansTC = Noto_Sans_TC({
 export const mPlusRounded1c = M_PLUS_Rounded_1c({
   display: 'swap',
   preload: true,
-  weight: ['100', '300', '400', '500', '700', '900'],
+  weight: ['300', '400', '500'],
   subsets: ['latin'],
   variable: '--font-m-plus-rounded-1c',
 });
